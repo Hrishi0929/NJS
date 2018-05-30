1c1445726469635e93550ba2eb2ada3c93e6200b
Hrishikesh <hrishiw@koinearth.com>
2018-05-30 19:56:00 +0530

    updated weather app with the forecast.io api integration

--- nodeSection-5/app.js
+++ nodeSection-5/app.js
@@ -1,27 +1,43 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help','h')
-  .argv;
-//console.log(argv);
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help','h')
+//   .argv;
+// //console.log(argv);
 
-geocode.geocodeAddress(argv.address,(errorMessage,results) => {
-  if(errorMessage){
-    console.log(errorMessage);
+// geocode.geocodeAddress(argv.address,(errorMessage,results) => {
+//   if(errorMessage){
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+const request = require('request');
+
+request({
+  url : 'https://api.darksky.net/forecast/5389768bf3982229c2020a51db6112dd/13.10197,77.5863591',
+  json: true
+},(error, response,body) => {
+  if(!error && response.statusCode === 200){
+  //printing the temperature from the dark sky api
+  console.log(`temperature: ${body.currently.temperature}`);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
+
 });
 
+//5389768bf3982229c2020a51db6112dd
 
